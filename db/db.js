ec86bb9cfb900994ae621bd335bb190ffb6a7173
heilorg <lhs01201@gmail.com>
2020-02-17 19:50:44 +0900

    movie version complete

--- db/db.js
+++ db/db.js
@@ -1,31 +1,49 @@
-export const movie = [
+let movies = [
     {
         id: 1,
-        name: "heilorg",
-        age: 19,
-        gender: "male"
+        name: "스타워즈",
+        score: 9
     },
     {
         id: 2,
-        name: "heilorg",
-        age: 19,
-        gender: "male"
+        name: "어벤져스",
+        score: 8
     },
     {
         id: 3,
-        name: "heilorg",
-        age: 19,
-        gender: "male"
+        name: "영화 뭐 없나?",
+        score: 7
     },
     {
         id: 4,
-        name: "heilorg",
-        age: 19,
-        gender: "male"
+        name: "그러게 더 생각나는게 없네",
+        score: 6
     }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => id === person.id);
-    return filteredPeople[0];
+    const filteredMovies = movies.filter(movie => id === movie.id);
+    return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if (movies.length > cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
 };
