fdf78ee4592fd37ec3e8875cff0aaac136fdca74
heilorg <lhs01201@gmail.com>
2020-02-17 20:20:47 +0900

    use movie api complete

--- db/db.js
+++ db/db.js
@@ -1,49 +1,17 @@
-let movies = [
-    {
-        id: 1,
-        name: "스타워즈",
-        score: 9
-    },
-    {
-        id: 2,
-        name: "어벤져스",
-        score: 8
-    },
-    {
-        id: 3,
-        name: "영화 뭐 없나?",
-        score: 7
-    },
-    {
-        id: 4,
-        name: "그러게 더 생각나는게 없네",
-        score: 6
+import fetch from "node-fetch";
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
+
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if (limit > 0) {
+        REQUEST_URL += "limit=" + limit;
     }
-];
-
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => id === movie.id);
-    return filteredMovies[0];
-};
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
+    if (rating > 0) {
+        REQUEST_URL += "&minimum_rating=" + rating;
     }
-};
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
+    return fetch(REQUEST_URL)
+        .then(res => res.json())
+        .then(res => {
+            return res.data.movies;
+        });
 };
